a616a9adad7086251685ae6ba3555a90bba3f8f2
Keiry <keiryrivera2022_gmail.com>
2021-04-27 21:29:09 -0600

    Lo subimos a la web

---
+++ js/menu.js
@@ -0,0 +1,41 @@
+var btnMenu = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
+btnMenu.addEventListener('click',function(){
+   nav.classList.toggle('muestrate');
+})
+/*----- PROMOCIONES DE BODAS -----*/
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideToggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#flip1").click(function(){
+    $("#panel1").slideToggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#flip2").click(function(){
+    $("#panel2").slideToggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#flip3").click(function(){
+    $("#panel3").slideToggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#flip4").click(function(){
+    $("#panel4").slideToggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#flip5").click(function(){
+    $("#panel5").slideToggle();
+  });
+});
